a1af333f08a5f28f51f09bc65420e68c134ac2f1
LMNT742 <bartkomrtn@gmail.com>
2024-03-02 21:00:01 +0100

    Doplnenie vypoctu ver/hor orientacie + doplnenie kodu pre zapis prikazov

--- scripts/detection/cam.py
+++ scripts/detection/cam.py
@@ -11,9 +11,9 @@ from std_msgs.msg import String, UInt16MultiArray
 
 model_path = "/home/martin/catkin_ws/src/yumi_controller/scripts/detection/weights/example_base_25_290909.pth"
 config = "example_base_config"
+threshold = 0.7
 
-
-# Detekcia objektov
+# Object detection
 def detect_objects(frame):
     with torch.no_grad():
         img_tensor = transforms.ToTensor()(frame).cuda().unsqueeze(0)
@@ -27,14 +27,15 @@ def detect_objects(frame):
         return classes, scores, boxes, masks
     
     
-# Definicia callbacku pre subscribera
+# Callback definition for subscriber
 def voice_callback(msg: String):
     received_bytes = msg.data.encode('utf-8')
     decoded_text = received_bytes.decode('utf-8')
     rospy.loginfo(decoded_text)
 
+    # Set class for detection
     if decoded_text == "kľúč":
-        target_class = "Wrench"  # nastavte cieľovú triedu
+        target_class = "Wrench"
     elif decoded_text == "kladivo":
         target_class = "Hammer"
     elif decoded_text == "kliešte":
@@ -45,15 +46,13 @@ def voice_callback(msg: String):
         target_class = "Flat_Screwdriver"
     else:
         target_class = "Nepoznam objekt"
-
-    threshold = 0.7  # prah pre skóre detekcie
+    # Detection threshold
     
     try:
-        # Detekcia objektov
         classes, scores, boxes, masks = detect_objects(frame)
         
-        # Overenie prítomnosti cieľového objektu
-        state, pos = get_pos(boxes, classes, target_class, frame.shape, threshold)
+        # Searching for selected equipment
+        state, pos = get_pos(boxes, classes, target_class, threshold)
         if state:
             pub.publish(pos)
             rospy.loginfo(f"{target_class} je prítomný na snímke.")
@@ -64,25 +63,33 @@ def voice_callback(msg: String):
         rospy.loginfo("Chyba pri spracovaní snímky.")
 
 
-# Vypocet stredu objektu
-def get_pos(boxes, classes, target_class, frame_shape, threshold):
+# Mid bbox counting
+def get_pos(boxes, classes, target_class, threshold):
     pos_list = []
+    width = x2 - x1
+    height = y2 - y1
     
     for i in range(len(classes)):
         class_name = cfg.dataset.class_names[classes[i]]
         score = scores[i]
         box = boxes[i]
         
-        if class_name == target_class and score > threshold:  # Ak nájdeme cieľový objekt
-            x1, y1, x2, y2 = int(box[0] * frame_shape[1]), int(box[1] * frame_shape[0]), int(box[2] * frame_shape[1]), int(box[3] * frame_shape[0])
-            center_x = int(x1 + x2) // 2
+        if class_name == target_class and score > threshold:
+            center_x = (x1 + x2) // 2
             center_y = (y1 + y2) // 2
-            pos_list.append((center_x, center_y))
+            # Vertical orientation
+            if width < height:
+                orientation = 0
+            # Horizontal orientation
+            elif height < width:
+                orientation = 1
+
+            pos_list.append((center_x, center_y, orientation))
 
     if pos_list:
         list_of_pos = UInt16MultiArray()
         list_of_pos.data = pos_list[0]
-        return True, list_of_pos  # Ak máme pozície, vrátime prvú pozíciu
+        return True, list_of_pos
     else:
         return False, None
 
@@ -104,11 +111,11 @@ with torch.no_grad():
     cfg.mask_proto_debug = False
 
 if __name__ == "__main__":
-    # definovanie subscribera
+    # Subscriber/Publisher definition
     rospy.init_node("object_detection")
     sub = rospy.Subscriber("/voice_reg", String, voice_callback)
     pub = rospy.Publisher("/object_reg", UInt16MultiArray, queue_size=10)
-    # Nastavenie kamery
+    # Camera setup
     cap = cv2.VideoCapture(4)
 
     start_time = time.time()
@@ -122,15 +129,14 @@ if __name__ == "__main__":
         frame_count += 1
 
         try:
-            # Detekcia objektov
             classes, scores, boxes, masks = detect_objects(frame)
 
             for i in range(len(classes)):
                 class_name = cfg.dataset.class_names[classes[i]]
                 score = scores[i]
                 box = boxes[i]
-                
-                if score > 0.7:  #definovanie zhody
+                # Displaying bboxes
+                if score > threshold:
                     x1, y1, x2, y2 = int(box[0] * frame.shape[1]), int(box[1] * frame.shape[0]), int(box[2] * frame.shape[1]), int(box[3] * frame.shape[0])
                     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
                     label = f"{class_name}: {score:.2f}"
@@ -139,19 +145,17 @@ if __name__ == "__main__":
         except TypeError:
             pass
         
-        # Zobrazenie snimkov za sekundu
+        # FPS counting
         fps_end_time = time.time()
         fps_elapsed_time = fps_end_time - start_time
         fps = frame_count / fps_elapsed_time if fps_elapsed_time > 0 else 0
 
         cv2.putText(frame, f"FPS: {fps:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
         cv2.imshow('YOLACT Object Detection', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-        # Kalkulacia FPS
         if frame_count >= 20:
             frame_count = 0
             start_time = time.time()
---
+++ scripts/detection/typein.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import String
+
+
+if __name__ == "__main__":
+    #definovanie publishera pre odosielanie sprav
+    command = ""
+    rospy.init_node("type_in_node")
+    pub = rospy.Publisher("/voice_reg", String, queue_size=10)
+    
+    while command != "koniec":
+
+        command = input("Zadaj naradie:")
+
+        if command != "":
+            pub.publish(command)
+            rospy.loginfo(command)
